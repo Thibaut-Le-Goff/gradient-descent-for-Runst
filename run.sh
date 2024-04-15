a8c3910e327d6e7d4e6ebb74413b925809ff62d1
Thibaut Le Goff <thibaut.le.goff27@gmail.com>
2024-04-15 22:10:10 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-rm -r bin
-rm logs/outputs/*.txt
-rm logs/errors/*.txt
+bin_path="bin"
+logs_path="logs"
+
+
+if [ -e "$bin_path" ]; then
+    rm -r bin
+else
+    echo "There is no binary files."
+fi
+
+if [ -e "$logs_path" ]; then
+    echo "There is already the logs folders."
+else
+    mkdir logs
+    mkdir logs/outputs
+    mkdir logs/errors    
+fi
 
 make && ./bin/test > logs/outputs/output.txt 2> logs/errors/error.txt
 make valgrind > logs/outputs/output_valgrind.txt 2> logs/errors/error_valgrind.txt
